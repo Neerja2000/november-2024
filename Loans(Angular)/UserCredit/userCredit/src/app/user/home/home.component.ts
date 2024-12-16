26c1e4a951e89a10fc5a6fab0f30cce2cbded7a3
sumalitakhi <sumalitakhi5260@gmail.com>
2024-12-16 18:05:21 +0530

    jjjjjj

--- Loans(Angular)/UserCredit/userCredit/src/app/user/home/home.component.ts
+++ Loans(Angular)/UserCredit/userCredit/src/app/user/home/home.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserLoginService } from 'src/app/shared/userLogin/user-login.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  isCreditApplied: boolean = false; // Flag to track if credit has been applied
 
+  constructor(private userLoginService: UserLoginService) {}
+
+  ngOnInit(): void {
+    this.fetchCreditApplications();
+  }
+
+  fetchCreditApplications(): void {
+    this.userLoginService.getCreditApplications().subscribe({
+      next: (data) => {
+        if (data.creditApplications && data.creditApplications.length > 0) {
+          this.isCreditApplied = true;
+        } else {
+          this.isCreditApplied = false;
+        }
+      },
+      error: (err) => {
+        console.error('Error fetching credit applications:', err);
+      }
+    });
+  }
 }
