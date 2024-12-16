cf4a1cc341c7bfbcb0e09dd0be0bb6ef2d4e5ebe
sumalitakhi <sumalitakhi5260@gmail.com>
2024-12-16 10:55:43 +0530

    jjjj

--- Loans(Angular)/UserCredit/userCredit/src/app/user/layout/header/header.component.ts
+++ Loans(Angular)/UserCredit/userCredit/src/app/user/layout/header/header.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/shared/auth/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  isLoggedIn: boolean = false; // Define the isLoggedIn property
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    // Check if the user is logged in
+    this.isLoggedIn = !!this.authService.getToken(); // Convert token presence to a boolean
+  }
+
+  logout(): void {
+    this.authService.removedata(); // Clear token
+    this.isLoggedIn = false; // Update isLoggedIn status
+    console.log('User logged out');
+  }
 }
