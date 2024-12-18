f8f2324ecab4707f91dea15c58874090fab4a18e
sumalitakhi <sumalitakhi5260@gmail.com>
2024-12-18 10:49:46 +0530

    hh

--- Loans(Angular)/UserCredit/userCredit/src/app/user/view-credit-status/view-credit-status.component.ts
+++ Loans(Angular)/UserCredit/userCredit/src/app/user/view-credit-status/view-credit-status.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserLoginService } from 'src/app/shared/userLogin/user-login.service';
 
-
 @Component({
   selector: 'app-view-credit-status',
   templateUrl: './view-credit-status.component.html',
@@ -10,7 +9,7 @@ import { UserLoginService } from 'src/app/shared/userLogin/user-login.service';
 export class ViewCreditStatusComponent implements OnInit {
   creditApplications: any[] = []; // Store API response
 
-  constructor(private userLoginService:UserLoginService) {}
+  constructor(private userLoginService: UserLoginService) {}
 
   ngOnInit(): void {
     this.fetchCreditApplications();
@@ -21,7 +20,7 @@ export class ViewCreditStatusComponent implements OnInit {
       next: (data) => {
         this.creditApplications = data.creditApplications.map((application: any) => ({
           ...application,
-          identity_proof: `http://194.238.17.235:7700/${application.identity_proof}`, 
+          identity_proof: `http://194.238.17.235:7700/${application.identity_proof}`,
         }));
       },
       error: (err) => {
@@ -29,5 +28,14 @@ export class ViewCreditStatusComponent implements OnInit {
       },
     });
   }
-}
 
+  // Check if there are any approved applications
+  hasApprovedApplications(): boolean {
+    return this.creditApplications.some(app => app.status === 'Approved');
+  }
+
+  // Check if there are any applications requiring review
+  hasReviewRequiredApplications(): boolean {
+    return this.creditApplications.some(app => app.status === 'Review Required');
+  }
+}
