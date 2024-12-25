a6d359367c11e77ab8cc56162fdd96d40b197a20
sumalitakhi <sumalitakhi5260@gmail.com>
2024-12-25 12:15:35 +0530

    Update banner.service.ts

--- Loans(Angular)/AdminCredit/adminCredits/src/app/shared/banner/banner.service.ts
+++ Loans(Angular)/AdminCredit/adminCredits/src/app/shared/banner/banner.service.ts
@@ -18,5 +18,31 @@ export class BannerService {
     const headers = new HttpHeaders().set('auth-token', this.token);
     return this.http.post(`${this.bannerbaseurl}/banners`, formData, { headers });
 }
+ 
+ 
+ 
+ 
+ 
+getBanners() {
+  const headers = new HttpHeaders().set('auth-token', this.token);
+  return this.http.get(this.bannerbaseurl + '/banners', { headers });
+}
+ 
+toggleBannerStatus(bannerId: number, isActive: boolean) {
+  const headers = new HttpHeaders().set('auth-token', this.token);
+  const body = { is_active: isActive };
+  return this.http.patch(
+    `${this.bannerbaseurl}/banners/${bannerId}/toggle`,
+    body,
+    { headers }
+  );
+}
+
+ 
 
 }
+ 
+ 
+ 
+ 
+ 
