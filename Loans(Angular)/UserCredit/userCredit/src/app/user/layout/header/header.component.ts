3814a9b21dcad4575b7892d808b17a9828925289
sumalitakhi <sumalitakhi5260@gmail.com>
2024-12-31 13:38:15 +0530

    ss

--- Loans(Angular)/UserCredit/userCredit/src/app/user/layout/header/header.component.ts
+++ Loans(Angular)/UserCredit/userCredit/src/app/user/layout/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/shared/auth/auth.service';
+import { UserLoginService } from 'src/app/shared/userLogin/user-login.service';
 
 @Component({
   selector: 'app-header',
@@ -8,17 +9,32 @@ import { AuthService } from 'src/app/shared/auth/auth.service';
 })
 export class HeaderComponent implements OnInit {
   isLoggedIn: boolean = false; // Define the isLoggedIn property
-
-  constructor(private authService: AuthService) {}
+  isCreditApplied: boolean = false;
+  constructor(private authService: AuthService,private userLoginService: UserLoginService) {}
 
   ngOnInit(): void {
     // Check if the user is logged in
     this.isLoggedIn = !!this.authService.getToken(); // Convert token presence to a boolean
+    this.fetchCreditApplications();
   }
 
   logout(): void {
     this.authService.removedata(); // Clear token
     this.isLoggedIn = false; // Update isLoggedIn status
     console.log('User logged out');
+  }
+  fetchCreditApplications(): void {
+    this.userLoginService.getCreditApplications().subscribe({
+      next: (data) => {
+        if (data.creditApplications && data.creditApplications.length > 0) {
+          this.isCreditApplied = true;
+        } else {
+          this.isCreditApplied = false;
+        }
+      },
+      error: (err) => {
+        console.error('Error fetching credit applications:', err);
+      }
+    });
   }
 }
