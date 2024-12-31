85d57ee0284602d1bdfd74b0602d4e8fd1d3736b
sumalitakhi <sumalitakhi5260@gmail.com>
2024-12-31 18:34:14 +0530

    hhhhhhh

--- Loans(Angular)/UserCredit/userCredit/src/app/user/layout/footer/footer.component.ts
+++ Loans(Angular)/UserCredit/userCredit/src/app/user/layout/footer/footer.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserLoginService } from 'src/app/shared/userLogin/user-login.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+ // Define the isLoggedIn property
+  isCreditApplied: boolean = false;
+  constructor(private userLoginService: UserLoginService) {}
 
+  ngOnInit(): void {
+
+    this.fetchCreditApplications();
+  }
+
+  fetchCreditApplications(): void {
+    this.userLoginService.getCreditApplications().subscribe({
+      next: (data) => {
+        if (data.creditApplications && data.creditApplications.length > 0) {
+          this.isCreditApplied = true;
+        } else {
+          this.isCreditApplied = false;
+        }
+      },
+      error: (err) => {
+        console.error('Error fetching credit applications:', err);
+      }
+    });
+  }
 }
